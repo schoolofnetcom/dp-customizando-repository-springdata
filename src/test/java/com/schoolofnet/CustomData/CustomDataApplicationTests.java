1d97de6900c6bc3f5290f1d518ce3af8d694b10c
Leonan Luppi <leonanluppi@Leonans-MacBook-Pro.local>
2021-05-11 15:54:40 -0300

    init

---
+++ src/test/java/com/schoolofnet/CustomData/CustomDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.CustomData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
